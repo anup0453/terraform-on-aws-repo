07d83e44de5e051e9a6633dccb53560f1efcac37
anup0453 <101499158+anup0453@users.noreply.github.com>
2023-01-03 14:45:25 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,14 +1,16 @@
 provider "aws" {
-  region = var.aws_region
+        access_key = "AKIAYDH5SEQT2NR3YHNP"
+        secret_key = "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"
+        region = var.aws_region
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
+        bucket_prefix = var.bucket_prefix
+        acl = var.acl
+
    versioning {
     enabled = var.versioning
   }
-  
+
   tags = var.tags
 }
